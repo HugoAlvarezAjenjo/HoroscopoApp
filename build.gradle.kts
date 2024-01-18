231fbf36931dadadd1ed8afef23c4da413e5ce62
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2024-01-18 12:02:48 +0100

    build: Add navigation safeargs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,6 @@
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id ("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.1" apply false
 }
