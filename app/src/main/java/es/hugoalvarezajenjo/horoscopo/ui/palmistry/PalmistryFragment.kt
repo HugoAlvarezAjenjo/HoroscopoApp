73631c82cecc8dff981131d66478a0b750826ccd
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2024-01-31 13:51:26 +0100

    feat: Camera permission management

--- app/src/main/java/es/hugoalvarezajenjo/horoscopo/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/es/hugoalvarezajenjo/horoscopo/ui/palmistry/PalmistryFragment.kt
@@ -1,25 +1,60 @@
 package es.hugoalvarezajenjo.horoscopo.ui.palmistry
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import dagger.hilt.android.AndroidEntryPoint
 import es.hugoalvarezajenjo.horoscopo.databinding.FragmentPalmistryBinding
 
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
+    companion object {
+        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
+    }
+
     private var _binding: FragmentPalmistryBinding? = null;
     private val binding get() = _binding!!
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                TODO("Permission Granted")
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Acepta los permisos para una mayor experiencia",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (checkCameraPermission()) {
+            TODO("Permission Granted")
+        } else {
+            requestPermissionLauncher.launch(CAMERA_PERMISSION)
+        }
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    private fun checkCameraPermission(): Boolean {
+        return PermissionChecker.checkSelfPermission(
+            requireContext(), CAMERA_PERMISSION
+        ) == PermissionChecker.PERMISSION_GRANTED
     }
 
 }
