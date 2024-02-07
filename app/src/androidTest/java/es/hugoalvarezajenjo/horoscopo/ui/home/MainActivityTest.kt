f1217d4a554fea98309ee696c5cfc4233161a75e
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2024-02-07 15:37:19 +0100

    test: UI testing Hell

---
+++ app/src/androidTest/java/es/hugoalvarezajenjo/horoscopo/ui/home/MainActivityTest.kt
@@ -0,0 +1,61 @@
+package es.hugoalvarezajenjo.horoscopo.ui.home
+
+import androidx.recyclerview.widget.RecyclerView
+import es.hugoalvarezajenjo.horoscopo.R
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import es.hugoalvarezajenjo.horoscopo.ui.detail.HoroscopeDetailActivity
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
+class MainActivityTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    var mainActivityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+        Intents.init()
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
+    }
+
+    @Test
+    fun when_activity_is_created_then_open_luck_fragment() {
+        onView(withId(R.id.luckFragment)).perform(click())
+    }
+
+    @Test
+    fun when_horoscope_is_selected_then_open_detail() {
+        onView(withId(R.id.rvHoroscope)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        intended(hasComponent(HoroscopeDetailActivity::class.java.name))
+    }
+
+}
