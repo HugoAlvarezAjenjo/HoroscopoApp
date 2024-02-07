e391be3b9b2d1f96853ecb9cef4591594cf6bb84
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2024-02-07 14:19:00 +0100

    build: Add MockK dependence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     //UnitTesting
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
+    testImplementation("io.mockk:mockk:1.12.3")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
