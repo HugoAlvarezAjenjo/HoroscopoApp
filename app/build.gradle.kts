5d3939e7fd141ab7a621f18e57e118c56f646701
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2024-02-07 13:33:24 +0100

    build: Add kotlin test runner dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,11 +71,16 @@ dependencies {
     implementation ("androidx.camera:camera-view:${cameraVersion}")
     implementation ("androidx.camera:camera-extensions:${cameraVersion}")
 
+    //Android Core
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //UnitTesting
     testImplementation("junit:junit:4.13.2")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
